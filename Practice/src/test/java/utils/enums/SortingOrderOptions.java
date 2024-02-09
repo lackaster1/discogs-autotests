55493cccb914a3afe9c7acb8d2e81e1d7eb6d01b
svatoslavk <mail66mail@mail.ru>
2024-02-10 00:44:09 +0100

    initial commit

--- Practice/src/test/java/utils/enums/SortingOrderOptions.java
+++
@@ -1,6 +0,0 @@
-package utils.enums;
-
-public enum SortingOrderOptions {
-    asc,
-    desc
-}
--- Practice/src/test/java/utils/models/Member.java
+++
@@ -1,8 +0,0 @@
-package utils.models;
-
-public class Member {
-    public boolean active;
-    public int id;
-    public String name;
-    public String resource_url;
-}
